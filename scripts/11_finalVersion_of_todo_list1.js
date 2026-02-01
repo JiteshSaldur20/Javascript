3bf054364d2119363918b7a0c56baf83c2d2d135
Jitesh Saldur <jiteshsaldur770@gmail.com>
2026-02-01 18:17:20 +0530

    Added file

---
+++ scripts/11_finalVersion_of_todo_list1.js
@@ -0,0 +1,47 @@
+const todoArray = JSON.parse(localStorage.getItem('todoArray')) || [{
+  name: 'make dinner',
+  dueDate: '2022-12-22'
+}, {
+  name: 'wash dishes',
+  dueDate: '2022-12-22'
+}];;
+
+function renderingArray(){
+  let displayArray = '';
+  for(let i=0; i<todoArray.length;i++){
+    const enteringVal = todoArray[i];
+    const {name, dueDate} = enteringVal;//destructring shortcut for const name = todoArray.name same for the dueDate variable
+    const html = `
+      <div>${name}</div>
+      <div>${dueDate}</div>
+      <button onclick="
+        todoArray.splice(${i}, 1);
+        renderingArray();
+        saveToStorage();
+      " class="delete-todo-button">Delete</button>
+    </div>
+    `;
+    displayArray += html;
+  }
+  document.querySelector('.js-display')
+    .innerHTML = displayArray;
+}
+
+function addingInput(){
+  const inputElement = document.querySelector('.js-input')
+  const inputElement1 = document.querySelector('.js-date')
+  const inputDate = inputElement1.value;
+  const insideInput = inputElement.value;
+
+  todoArray.push({
+    name: insideInput,//we can put only name if the property name and the variable name is same
+    dueDate: inputDate});
+  inputElement.value = '';
+
+  renderingArray();
+  saveToStorage();
+}
+
+function saveToStorage() {
+  localStorage.setItem('todoArray', JSON.stringify(todoArray));
+}
